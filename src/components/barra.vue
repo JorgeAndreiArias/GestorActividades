2aa28c0a0ab33c85f130f744f1bca018ef8c4581
Andrei Arias <andreiarias53@gmail.com>
2019-05-03 13:03:04 -0500

    Navbar Agregado

---
+++ src/components/barra.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="#"> {{ getName }}</a>
+            <div >
+                <form class="form-inline my-2 my-lg-0"  v-on:submit.prevent="logout">
+                    <button class="btn btn-outline-success" type="submit">Cerrar Sesión</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+       
+    
+</template>
+
+<script>
+
+export default {
+	 mounted: function(){
+    },
+    data: function(){
+		return{
+
+		}
+	}, 
+	methods:{ 
+		logout(){
+			this.$store.commit('auth', {});
+            this.$store.commit('user', {});
+            this.$store.commit('change', 1);
+            this.$cookies.remove('UserAuth');
+            this.$cookies.remove('Token');
+		},
+	},
+	computed:{
+		getName(){
+			var user =  this.$store.getters.user;;
+			return  user.Nombre + " " + user.Apellidos;
+		}
+	}
+}
+</script>
+
+<style scoped>
+</style> 
